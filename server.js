2364fb4c7b1ffc601e7660f096b44c5bf348a0e9
Glitch (universal-everything) <none>
2018-11-11 22:37:39 +0000

    🚉🍢 Updated with Glitch

--- server.js
+++ server.js
@@ -4,19 +4,23 @@
 // init project
 const express = require('express');
 const app = express();
+var phpExpress = require('php-express')({
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  // assumes php is in your PATH
+  binPath: 'php'
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+// set view engine to php-express
+app.set('views', './views');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('PHPExpress app listening at http://%s:%s', host, port);
 });
+
